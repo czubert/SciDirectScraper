1253af0548ea77468dd46db29e07d53efa25c562
czubert <czubercik@gmail.com>
2023-01-18 03:15:28 +0100

    getting urls works. need to apply another object to itarate pages or do it in selenium, simple loop with driver.get(offset)

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.edge.service import Service as EdgeService
+
+from article import Article
 
 
 class ScienceDirectParser:
@@ -18,13 +18,14 @@ class ScienceDirectParser:
         self.core_url = 'https://www.sciencedirect.com/search?qs='
         self.articles_urls = []
         self.soup = None
+        self.offset = None
 
     def create_url(self, page_num):
         years = [str(x) for x in self.years]
         url_years = "%2c".join(years)
         pub_per_page = self.pub_per_page * self.pub_per_page_multi25
-        offset = (page_num-1) * pub_per_page
-        self.parser_url = f'{self.core_url}{self.keyword}&years={url_years}&show={pub_per_page}&offset={offset}'
+        self.offset = (page_num - 1) * pub_per_page
+        self.parser_url = f'{self.core_url}{self.keyword}&years={url_years}&show={pub_per_page}&offset={self.offset}'
 
     def get_articles_urls(self):
         try:
@@ -33,23 +34,22 @@ class ScienceDirectParser:
             from webdriver_manager.microsoft import EdgeChromiumDriverManager
             driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
 
-        driver.get("https://www.sciencedirect.com/search?qs=SERS&show=25")
-        wait = WebDriverWait(driver, 5)
+        driver.get(self.parser_url)
+        wait = WebDriverWait(driver, 1)
 
         # store all the links in a list
         self.articles_urls += [item.get_attribute("href") for item in wait.until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, "subtype-srctitle-link")))]
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "result-list-title-link")))]
 
     def scrap(self):
         for page_num in range(1, self.n_pages + 1):
-            print(page_num)
             self.create_url(page_num)
             self.get_articles_urls()
-            # print(self.articles_urls)
-            # print(len(self.articles_urls))
-            print(self.parser_url)
+            for pub_url in self.articles_urls[:3]:
+                article = Article(pub_url)
+                article.parse_article()
 
 
 if __name__ == '__main__':
-    science = ScienceDirectParser(keyword='SERS', pub_per_page_multi25=2, n_pages=2, years=[2022, 2021])
+    science = ScienceDirectParser(keyword='SERS', pub_per_page_multi25=1, n_pages=1, years=[2022, 2021])
     science.scrap()
